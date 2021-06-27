958063ea4c1a9dea99a10e0107bce2f84a8133a3
Sergio Pacheco <sergiomiguelpacheco64@gmail.com>
2021-06-27 13:40:29 -0400

    Update message-retrieval.php

--- php/message-retrieval.php
+++ php/message-retrieval.php
@@ -1,13 +1,31 @@
 <?php
+	$errors = "";
+	$myemail = "sergiomiguelpacheco64@gmail.com";
+	
+	if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"]))
+	{
+		$errors .= "\n Error: all fields are required";
+	}
+
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$message = $_POST["message"];
 
-    $email_from = $email;
+	if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address))
+	{
+		$errors .= "\n Error: Invalid email address";
+	}
 
-	$email_subject = "Portfolio Site Message From: $name";
-
-	$email_body = $message;
-
-     mail("sergiomiguelpacheco64@gmail.com", $email_subject, $email_message);
+	if(empty($errors))
+	{
+		$to = $myemail;
+		$email_subject = "Portfolio Site Message From: $name";
+		$email_body = "$message".
+		
+		$headers = "From: $myemail\n";
+		
+		$headers .= "Reply-To: $email_address";
+		
+		mail($to, $email_subject, $email_body, $headers);
+	}
 ?>

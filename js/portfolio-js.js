151290f0c3ac7b0eefde7900a79581da0511ba03
Sergio Pacheco <sergiomiguelpacheco64@gmail.com>
2021-09-23 15:39:29 -0600

    Update portfolio-js.js

--- js/portfolio-js.js
+++ js/portfolio-js.js
@@ -39,5 +39,30 @@ $(".close-overlay-two").click(function ()
 // Initialization for the AOS CSS Animations
 AOS.init
 ({
-    duration: 800,
+    duration: 800
 });
+
+
+// Back to Top Button JS
+scrollBtn = document.getElementById("scrollToTopBtn");
+window.onscroll = function() 
+{
+    scrollFunction()
+};
+function scrollFunction() 
+{
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) 
+    {
+        scrollBtn.style.display = "block";
+    } 
+    else 
+    {
+        scrollBtn.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() 
+{
+    document.body.scrollTop = 0; // Safari Compatibility
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, & IE Compatibility
+}
